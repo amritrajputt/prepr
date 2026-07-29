ea1f61ea8a942a0d7f8174f61b6681d7d6bb7ed2
amrit <vishal179singh@gmail.com>
2026-07-29 23:11:45 +0530

    tanstack router setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,32 @@ import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/react'
 import { ThemeProvider } from './components/ThemeProvider'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
+
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY || ''}>
-       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+        <App />
       </ThemeProvider>
     </ClerkProvider>
   </StrictMode>,
