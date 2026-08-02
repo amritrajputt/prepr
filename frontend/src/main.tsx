2fc2975ad45e8f00fb46f879ed07ba3f399ffd56
amrit <vishal179singh@gmail.com>
2026-08-02 14:37:05 +0530

    removed strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -25,12 +25,12 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+
     <ClerkProvider publishableKey={PUBLISHABLE_KEY || ''}>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <RouterProvider router={router} />
         <App />
       </ThemeProvider>
     </ClerkProvider>
-  </StrictMode>,
+
 )
